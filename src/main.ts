37f5664f9202c51aba377bcea8432a39b4e1dda1
Alexandr Ivanin <ya@aivanin.ru>
2024-01-20 23:02:20 +0300

    feat: refactoring main.ts and draw world map

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
 import './style.css';
 import mWalkerPath from './assets/M-walk.png';
+import durotarPath from './assets/durotar.png';
+import worldMapJson from './assets/world-map.json';
 import { TCanvasParams } from './types/canvas-params.type';
 import { Direction } from './enums/direction.enum';
 import { Keys } from './enums/keys.enum';
 import { TCharacterParams } from './types/character-params.type';
+import { loadSprites } from './utils/load-sprites';
+import { loadImage } from './utils/load-image';
+import { drawMap } from './utils/drawMap';
+import { drawCharacter } from './utils/draw-character';
 
 const canvas = document.getElementById('game') as HTMLCanvasElement | null;
+const loader = document.querySelector('h3') as HTMLHeadElement;
 
 if (!canvas) throw new Error('Canvas not found');
 
 const canvasParams: TCanvasParams = {
   width: canvas.width,
   height: canvas.height,
-  ctx: canvas.getContext('2d'),
+  ctx: canvas.getContext('2d') as CanvasRenderingContext2D,
 };
+
+if (!canvasParams.ctx) throw new Error('Canvas context not found');
 
 const characterParams: TCharacterParams = {
   x: 0,
@@ -22,69 +31,44 @@ const characterParams: TCharacterParams = {
   direction: Direction.DOWN,
 };
 
-const FRAMES = 3;
-const COEFFICIENT = 48.5;
+loadSprites([loadImage(mWalkerPath), loadImage(durotarPath)]).then(
+  ([mWalker, durotar]) => {
+    loader.remove();
+    let lastTimeUpdate = 0;
 
-let lastTimeUpdate = 0;
-let step = 0;
-const img = new Image();
-img.src = mWalkerPath;
+    const { layers } = worldMapJson;
+    const { data: mainLayer } = layers[0];
+    const { data: forestLayer } = layers[1];
 
-const animate = (timestamp: number) => {
-  const deltaTime = timestamp - lastTimeUpdate;
-  const img = new Image();
-  img.src = mWalkerPath;
+    const animate = (timestamp: number) => {
+      const deltaTime = timestamp - lastTimeUpdate;
 
-  img.addEventListener('load', () => {
-    if (characterParams.keyPress) {
-      step = (step + 0.01 * deltaTime) % FRAMES;
-      switch (characterParams.direction) {
-        case Direction.UP:
-          characterParams.y -= 0.1 * deltaTime;
-          break;
-        case Direction.RIGHT:
-          characterParams.x += 0.1 * deltaTime;
-          break;
-        case Direction.DOWN:
-          characterParams.y += 0.1 * deltaTime;
-          break;
-        case Direction.LEFT:
-          characterParams.x -= 0.1 * deltaTime;
-          break;
-      }
+      drawMap({
+        data: mainLayer,
+        ctx: canvasParams.ctx,
+        img: durotar,
+      });
 
-      if (characterParams.x <= 0) {
-        characterParams.x = 0;
-      } else if (characterParams.x >= canvasParams.width - 64) {
-        characterParams.x = canvasParams.width - 64;
-      }
+      drawCharacter({
+        characterParams,
+        canvasParams,
+        deltaTime,
+        img: mWalker,
+      });
 
-      if (characterParams.y <= 0) {
-        characterParams.y = 0;
-      } else if (characterParams.y >= canvasParams.height - 64) {
-        characterParams.y = canvasParams.height - 64;
-      }
-    }
-    if (!canvasParams.ctx) throw new Error('No canvas context');
-    canvasParams.ctx.clearRect(0, 0, canvasParams.width, canvasParams.height);
-    canvasParams.ctx.drawImage(
-      img,
-      COEFFICIENT * Math.floor(step),
-      COEFFICIENT * characterParams.direction,
-      COEFFICIENT,
-      COEFFICIENT,
-      characterParams.x,
-      characterParams.y,
-      64,
-      64
-    );
-  });
+      drawMap({
+        data: forestLayer,
+        ctx: canvasParams.ctx,
+        img: durotar,
+      });
 
-  lastTimeUpdate = timestamp;
-  requestAnimationFrame(animate);
-};
+      lastTimeUpdate = timestamp;
+      requestAnimationFrame(animate);
+    };
 
-requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
+  }
+);
 
 const keyDownHandler = (event: KeyboardEvent) => {
   characterParams.keyPress = true;
